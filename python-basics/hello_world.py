1ebaee7f23a1662c303f4677dbb4d9ffcbe98383
ddauletbaev <ddauletbaev93@gmail.com>
2026-01-31 00:22:28 +0500

    Add existing Python Basics files including HOME

--- python-basics/hello_world.py
+++ python-basics/hello_world.py
@@ -1,18 +1,3 @@
-# Example 1: Basic Hello World
-print("Hello, world!")
-
-# Example 2: Hello with a variable
-greeting = "Hello, Python!"
-print(greeting)
-
-# Example 3: Concatenation
-name = "Damir"
-print("Hello, " + name)
-
-# Example 4: Using f-strings
-name = "Damir"
-age = 20
-print(f"My name is {name} and I am {age} years old.")
-
-# Example 5: Multi-line print
-print("Hello\nPython\nWorld")
+x = complex(3)
+y = complex(1, 2)
+print(x, y)
